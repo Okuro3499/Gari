85029241213b8cb93eb95d478d53c58a9c53ccd7
Gideon Okuro <gideonollonde@gmail.com>
2022-02-14 17:28:35 +0300

    upload image trial

--- app/src/main/java/com/justin/gari/activities/ProfileCompleteActivity.kt
+++ app/src/main/java/com/justin/gari/activities/ProfileCompleteActivity.kt
@@ -32,6 +32,9 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.cloudinary.android.MediaManager
+import com.cloudinary.android.callback.ErrorInfo
+import com.cloudinary.android.callback.UploadCallback
 import retrofit2.Retrofit
 import java.io.File
 
@@ -39,13 +42,19 @@ class ProfileCompleteActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
     private val sharedPrefFile = "sharedPrefData"
     private lateinit var apiClient: ApiClient
-
+    var config: HashMap<String, String> = HashMap()
+    var imgpath: String? = null
 
 //    var imageDriverLicensePicker:ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_complete)
+        config["cloud_name"] = "okuro"
+        config["api_key"] = "639124242572386"
+        config["api_secret"] = "jbUm8UuCamH5sRzonkKkV_3a1n8"
+        config["upload_preset"] = "default_preset"
+        MediaManager.init(this, config)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -62,20 +71,20 @@ class ProfileCompleteActivity : AppCompatActivity() {
         val landMark = findViewById<TextView>(R.id.tvLandMark)
 
         val imageDriverLicensePicker = findViewById<ImageView>(R.id.ivDl)
-        val imageNationalIdPicker = findViewById<ImageView>(R.id.ivId)
-        val imageUserPhotoPicker = findViewById<ImageView>(R.id.ivPhoto)
-
-        imageDriverLicensePicker.setOnClickListener{
-            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop().maxResultSize(210,150).start()
-        }
-
-        imageNationalIdPicker.setOnClickListener{
-            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop().maxResultSize(210,150).start()
-        }
-
-        imageUserPhotoPicker.setOnClickListener{
-            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop().maxResultSize(210,150).start()
-        }
+//        val imageNationalIdPicker = findViewById<ImageView>(R.id.ivId)
+//        val imageUserPhotoPicker = findViewById<ImageView>(R.id.ivPhoto)
+//
+//        imageDriverLicensePicker.setOnClickListener{
+//            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop().maxResultSize(210,150).start()
+//        }
+//
+//        imageNationalIdPicker.setOnClickListener{
+//            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop().maxResultSize(210,150).start()
+//        }
+//
+//        imageUserPhotoPicker.setOnClickListener{
+//            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop().maxResultSize(210,150).start()
+//        }
 
         val clientId = sharedPreferences.getString("client_id", "default")
 
@@ -167,7 +176,7 @@ class ProfileCompleteActivity : AppCompatActivity() {
 
         val dlButton = findViewById<Button>(R.id.btDlUpload)
         dlButton.setOnClickListener{
-
+            uploadToCloudinary(imgpath.toString())
 //            apiClient.getApiService(this).uploadDl().enqueue(object : Callback<UploadDlResponse> {
 //                override fun onResponse(call: Call<UploadDlResponse>, response: Response<UploadDlResponse>) {
 //                    if (response.isSuccessful) {
@@ -186,28 +195,88 @@ class ProfileCompleteActivity : AppCompatActivity() {
             val intent = Intent(this, DetailActivity::class.java)
             startActivity(intent)
         }
+
+        imageDriverLicensePicker.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()                    //Crop image(Optional), Check Customization for more option
+                .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
+
+
+    }
+
+    fun uploadToCloudinary(filepath: String) {
+        MediaManager.get().upload(filepath).unsigned("sample_app_preset").callback(object : UploadCallback {
+            override fun onSuccess(requestId: String?, resultData: MutableMap<Any?, Any?>?) {
+                Toast.makeText(applicationContext, "Task successful", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
+
+            }
+
+            override fun onReschedule(requestId: String?, error: ErrorInfo?) {
+
+            }
+
+            override fun onError(requestId: String?, error: ErrorInfo?) {
+                error
+                Toast.makeText(applicationContext, "Task Not successful"+ error, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStart(requestId: String?) {
+
+                Toast.makeText(applicationContext, "Start", Toast.LENGTH_SHORT).show()
+            }
+        }).dispatch()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val imageDriverLicensePicker = findViewById<ImageView>(R.id.ivDl)
-        val imageNationalIdPicker = findViewById<ImageView>(R.id.ivId)
-        val imageUserPhotoPicker = findViewById<ImageView>(R.id.ivPhoto)
         super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val fileUri = data?.data
 
-        if(resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
-            imageDriverLicensePicker?.setImageURI(data?.data)
+            imageDriverLicensePicker.setImageURI(fileUri)
 
+            //You can get File object from intent
+            val file: File = ImagePicker.getFile(data)!!
+
+            //You can also get File Path from intent
+            val filePath: String = ImagePicker.getFilePath(data)!!
+            imgpath = filePath
+            uploadToCloudinary(filePath)
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Log.e("cloudinary", "failed: ${ImagePicker.getError(data)}")
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
         }
-
-        if(resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
-            imageNationalIdPicker?.setImageURI(data?.data)
-        }
-
-        if(resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
-            imageUserPhotoPicker?.setImageURI(data?.data)
-        }
-
     }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        val imageDriverLicensePicker = findViewById<ImageView>(R.id.ivDl)
+//        val imageNationalIdPicker = findViewById<ImageView>(R.id.ivId)
+//        val imageUserPhotoPicker = findViewById<ImageView>(R.id.ivPhoto)
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if(resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
+//            imageDriverLicensePicker?.setImageURI(data?.data)
+//
+//        }
+//
+//        if(resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
+//            imageNationalIdPicker?.setImageURI(data?.data)
+//        }
+//
+//        if(resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
+//            imageUserPhotoPicker?.setImageURI(data?.data)
+//        }
+//
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
@@ -216,4 +285,6 @@ class ProfileCompleteActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 }
+
+
 
