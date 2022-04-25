b7074539bc8ee09f8f4ff8d5980fe7c6a2701f33
Okuro3499 <33450715Go>
2022-04-25 10:07:56 +0300

    payment ui

---
+++ app/src/main/java/com/justin/gari/activities/PaymentActivity.kt
@@ -0,0 +1,17 @@
+package com.justin.gari.activities
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.justin.gari.R
+
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+
+
+    }
+}
